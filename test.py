434202c0362e3b20092130a751d2baf2c3af4eef
964293341 <asdawej123@163.com>
2023-06-09 11:30:15 +0800

    enemy now shot from the top only
    test.py:
    
    - ADD enemy_init
    - ADD code comment in Chinese

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import pyconio as pco
 import pygraphics as pgp
 
 import math
+import random as rd
 
 # SIZE
 R, C = 25, 50
@@ -19,19 +20,26 @@ for i in range(R):
 background = pgp.PictureObj(background)
 background.hollow()
 
-buf = pgp.Buffers(0.01)
-obj = pgp.DynamicObj([['#']], row=1, col=1)
-enemy = pgp.DynamicObj([['@']], row=R-2, col=C-2)
-board = pgp.PaintBoard(R, C)
-board.paint(obj)
-board.paint(enemy)
-board.paint(background)
-board.render_flash(buf)
-while True:
-    enemy_mov_ang = math.atan2(obj.col-enemy.col, obj.row-enemy.row)
-    enemy_mov_len = 0.05
+buf = pgp.Buffers(0.01)                             # 缓冲区
+obj = pgp.DynamicObj([['#']], row=R-2, col=C//2)    # 自机
+enemy = pgp.DynamicObj([['@']], row=1, col=1)       # 敌机
+def enemy_init(enemy: pgp.DynamicObj, board: pgp.PaintBoard):
+    '敌机初始化, 随机取一个顶部位置射出'
+    board._erase(enemy) # 只是擦除敌机, 没有从画板上去除敌机
+    enemy.row = 1
+    enemy.col = rd.randint(1, C-2)
+    enemy_mov_ang = (rd.random()-0.5)*math.pi
+    enemy_mov_len = 0.25
     enemy.move_r = enemy_mov_len*math.cos(enemy_mov_ang)
     enemy.move_c = enemy_mov_len*math.sin(enemy_mov_ang)
+board = pgp.PaintBoard(R, C)
+board.paint(background)
+board.paint(obj)
+board.paint(enemy)
+enemy_init(enemy, board)
+board.render_flash(buf)
+while True:
+    # 检测键盘
     if pco.py_kbhit():
         c = pco.py_getch()
         if c == 119:
@@ -42,11 +50,15 @@ while True:
             obj.move_c = -1
         if c == 100:
             obj.move_c = 1
+    # 撞上敌机
     if board.detect_obj(obj, enemy):
         break
+    # 自机碰到边界
     if board.detect_border(obj) or board.detect_obj(obj, background):
         obj.move_r = obj.move_c = 0
+    # 敌机碰到边界
     if board.detect_border(enemy) or board.detect_obj(enemy, background):
         enemy.move_r = enemy.move_c = 0
+        enemy_init(enemy, board)
     board.render_flash(buf)
     obj.move_r = obj.move_c = 0
