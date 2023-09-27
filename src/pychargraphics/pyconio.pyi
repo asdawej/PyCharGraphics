2386fdecd9491aa9780dc1b9c72b9b30bb46dd51
964293341 <asdawej123@163.com>
2023-09-27 17:23:26 +0800

    ADD pyconio.pyi

---
+++ src/pychargraphics/pyconio.pyi
@@ -0,0 +1,12 @@
+# -------------------- #
+# __author__ = asdawej #
+# -------------------- #
+
+def py_kbhit() -> bool:
+    '`_kbhit` in C Language'
+    ...
+
+
+def py_getch() -> int:
+    '`_getch` in C Language'
+    ...
